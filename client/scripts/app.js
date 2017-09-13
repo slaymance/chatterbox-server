fad2829acc61c8873b56be2767ada14a1c6046b0
Shane Laymance <shane.laymance@gmail.com>
2017-09-13 16:31:35 -0700

    Implement communication on server

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -22,7 +22,7 @@
 
 app = {
 
-  server: 'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages',
+  server: 'http://127.0.0.1:3000/classes/messages',
 
   init: function() {
     // Get username
@@ -79,6 +79,7 @@ app = {
       data: { order: '-createdAt' },
       contentType: 'application/json',
       success: function(json) {
+        console.log(json, 'success');
         app.displayMessages(json.results);
       },
       complete: function() {
@@ -116,107 +117,107 @@ app = {
 };
 
 
-/////////////////////////////////////////////////////////////////////////////
-// Backbone-based Implementation of (minimal) chatterbox client
-/////////////////////////////////////////////////////////////////////////////
+// /////////////////////////////////////////////////////////////////////////////
+// // Backbone-based Implementation of (minimal) chatterbox client
+// /////////////////////////////////////////////////////////////////////////////
 
-var Message = Backbone.Model.extend({
-  url: 'https://api.parse.com/1/classes/messages/',
-  defaults: {
-    username: '',
-    text: ''
-  }
-});
+// var Message = Backbone.Model.extend({
+//   url: 'https://api.parse.com/1/classes/messages/',
+//   defaults: {
+//     username: '',
+//     text: ''
+//   }
+// });
 
-var Messages = Backbone.Collection.extend({
+// var Messages = Backbone.Collection.extend({
 
-  model: Message,
-  url: 'https://api.parse.com/1/classes/messages/',
+//   model: Message,
+//   url: 'https://api.parse.com/1/classes/messages/',
 
-  loadMsgs: function() {
-    this.fetch({data: { order: '-createdAt' }});
-  },
+//   loadMsgs: function() {
+//     this.fetch({data: { order: '-createdAt' }});
+//   },
 
-  parse: function(response, options) {
-    var results = [];
-    for (var i = response.results.length - 1; i >= 0; i--) {
-      results.push(response.results[i]);
-    }
-    return results;
-  }
+//   parse: function(response, options) {
+//     var results = [];
+//     for (var i = response.results.length - 1; i >= 0; i--) {
+//       results.push(response.results[i]);
+//     }
+//     return results;
+//   }
 
-});
+// });
 
-var FormView = Backbone.View.extend({
+// var FormView = Backbone.View.extend({
 
-  initialize: function() {
-    this.collection.on('sync', this.stopSpinner, this);
-  },
+//   initialize: function() {
+//     this.collection.on('sync', this.stopSpinner, this);
+//   },
 
-  events: {
-    'submit #send': 'handleSubmit'
-  },
+//   events: {
+//     'submit #send': 'handleSubmit'
+//   },
 
-  handleSubmit: function(e) {
-    e.preventDefault();
+//   handleSubmit: function(e) {
+//     e.preventDefault();
 
-    this.startSpinner();
+//     this.startSpinner();
 
-    var $text = this.$('#message');
-    this.collection.create({
-      username: window.location.search.substr(10),
-      text: $text.val()
-    });
-    $text.val('');
-  },
+//     var $text = this.$('#message');
+//     this.collection.create({
+//       username: window.location.search.substr(10),
+//       text: $text.val()
+//     });
+//     $text.val('');
+//   },
 
-  startSpinner: function() {
-    this.$('.spinner img').show();
-    this.$('form input[type=submit]').attr('disabled', 'true');
-  },
+//   startSpinner: function() {
+//     this.$('.spinner img').show();
+//     this.$('form input[type=submit]').attr('disabled', 'true');
+//   },
 
-  stopSpinner: function() {
-    this.$('.spinner img').fadeOut('fast');
-    this.$('form input[type=submit]').attr('disabled', null);
-  }
+//   stopSpinner: function() {
+//     this.$('.spinner img').fadeOut('fast');
+//     this.$('form input[type=submit]').attr('disabled', null);
+//   }
 
-});
+// });
 
-var MessageView = Backbone.View.extend({
+// var MessageView = Backbone.View.extend({
 
-  initialize: function() {
-    this.model.on('change', this.render, this);
-  },
+//   initialize: function() {
+//     this.model.on('change', this.render, this);
+//   },
 
-  template: _.template('<div class="chat" data-id="<%- objectId %>"> \
-                          <div class="user"><%- username %></div> \
-                          <div class="text"><%- text %></div> \
-                        </div>'),
+//   template: _.template('<div class="chat" data-id="<%- objectId %>"> \
+//                           <div class="user"><%- username %></div> \
+//                           <div class="text"><%- text %></div> \
+//                         </div>'),
 
-  render: function() {
-    this.$el.html(this.template(this.model.attributes));
-    return this.$el;
-  }
+//   render: function() {
+//     this.$el.html(this.template(this.model.attributes));
+//     return this.$el;
+//   }
 
-});
+// });
 
-var MessagesView = Backbone.View.extend({
+// var MessagesView = Backbone.View.extend({
 
-  initialize: function() {
-    this.collection.on('sync', this.render, this);
-    this.onscreenMessages = {};
-  },
+//   initialize: function() {
+//     this.collection.on('sync', this.render, this);
+//     this.onscreenMessages = {};
+//   },
 
-  render: function() {
-    this.collection.forEach(this.renderMessage, this);
-  },
+//   render: function() {
+//     this.collection.forEach(this.renderMessage, this);
+//   },
 
-  renderMessage: function(message) {
-    if (!this.onscreenMessages[message.get('objectId')]) {
-      var messageView = new MessageView({model: message});
-      this.$el.prepend(messageView.render());
-      this.onscreenMessages[message.get('objectId')] = true;
-    }
-  }
+//   renderMessage: function(message) {
+//     if (!this.onscreenMessages[message.get('objectId')]) {
+//       var messageView = new MessageView({model: message});
+//       this.$el.prepend(messageView.render());
+//       this.onscreenMessages[message.get('objectId')] = true;
+//     }
+//   }
 
-});
+// });
